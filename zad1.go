c94ff6153b922fcc2a5cc1a6d6122c6b83a851f2
Adam Chojnacki <adam.chojnacki.syl@gmail.com>
2022-03-06 14:10:46 +0100

    Add some probs counting

--- zad1.go
+++ zad1.go
@@ -18,22 +18,32 @@ func main() {
 func scanFile(path string) {
 
 	counterSlice := make([]int, 256)
+	counterSlice2 := make([][]int, 256)
 	probs1 := make([]float64, 256)
+	probs2 := make([][]float64, 256)
+
+	for i := range probs2 {
+		probs2[i] = make([]float64, 256)
+		counterSlice2[i] = make([]int, 256)
+	}
 	counter := 0
 
 	f, err := os.Open(path)
 	check(err)
 
+	var prevSymbol byte
 	currSymbol := make([]byte, 1)
 	// prevSymbol := make([]byte, 1)
 
 	for {
+		prevSymbol = currSymbol[0]
 		control, _ := f.Read(currSymbol)
 		if control == 0 {
 			break
 		}
 		counter++
 		counterSlice[currSymbol[0]]++
+		counterSlice2[prevSymbol][currSymbol[0]]++
 		// fmt.Println((currSymbol))
 	}
 
@@ -41,8 +51,11 @@ func scanFile(path string) {
 		probs1[i] = float64(k) / float64(counter)
 	}
 
-	fmt.Println(counterSlice)
-	fmt.Println(probs1)
+	for _, k := range counterSlice2 {
+		fmt.Println(k)
+		fmt.Println()
+	}
+	//fmt.Println(probs1)
 	// fmt.Println((prevSymbol))
 
 	defer f.Close()
