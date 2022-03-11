27242c74d07a64dacba9cb80b87430d9a84479ae
Adam Chojnacki <adam.chojnacki.syl@gmail.com>
2022-03-11 10:51:13 +0100

    Clean some code, add sample mp3

--- zad1.go
+++ zad1.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 )
 
@@ -25,6 +26,11 @@ func scanFile(path string) {
 	for i := range probs2 {
 		probs2[i] = make([]float64, 256)
 		counterSlice2[i] = make([]int, 256)
+
+		for j := range probs2[i] {
+			probs2[i][j] = 0.0
+			counterSlice2[i][j] = 0
+		}
 	}
 	counter := 0
 
@@ -51,12 +57,47 @@ func scanFile(path string) {
 		probs1[i] = float64(k) / float64(counter)
 	}
 
-	for _, k := range counterSlice2 {
-		fmt.Println(k)
-		fmt.Println()
+	//index i, j holds value of P(j|i)
+	for i, k := range counterSlice2 {
+		for j, l := range k {
+			if l != 0 && counterSlice[i] != 0 {
+				probs2[i][j] = float64(l) / float64(counterSlice[i])
+			} else {
+				probs2[i][j] = 0.0
+			}
+		}
 	}
-	//fmt.Println(probs1)
-	// fmt.Println((prevSymbol))
+
+	//COUNT H(Y|X)
+	HYX := 0.0
+	for j := 0; j < 256; j++ {
+
+		//COUNT H(Y|x)
+		HYx := 0.0
+		for i := 0; i < 256; i++ {
+			Pyx := probs2[i][j]
+			if Pyx != 0.0 {
+				//fmt.Print(" ", Pyx)
+				Iyx := -math.Log2(Pyx)
+				HYx += Pyx * Iyx
+			}
+
+		}
+
+		HYX += probs1[j] * HYx
+	}
+
+	H := 0.0
+
+	for i := 0; i < 256; i++ {
+		Px := probs1[i]
+		if Px != 0.0 {
+			Ix := -math.Log2(Px)
+			H += Px * Ix
+		}
+	}
 
 	defer f.Close()
+	fmt.Println("HYX = ", HYX)
+	fmt.Println("H = ", H)
 }
